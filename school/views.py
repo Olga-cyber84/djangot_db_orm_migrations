7f7943990b5d1cacc7a0c500ad6dc946b6c28e06
Olga-cyber84 <doorsdream007@ya.ru>
2023-05-15 23:10:50 +0300

    first commit

---
+++ school/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    object_list = Student.objects.all()
+    context = {'object_list': object_list}
+    ordering = 'group'
+    my_list = Student.objects.values('teachers', ordering)
+    print('***')
+    print(my_list)
+
+    return render(request, template, context)
